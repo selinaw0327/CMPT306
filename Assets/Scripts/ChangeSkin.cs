b73f3661e9317b62377023a830800a99d82fd454
AdamBaker01134 <72470135+AdamBaker01134@users.noreply.github.com>
2020-11-12 18:25:09 -0600

    Added Animator Override Controller and a script to change skins

---
+++ Assets/Scripts/ChangeSkin.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSkin : MonoBehaviour
+{
+
+    public AnimatorOverrideController copperAnim;
+    public AnimatorOverrideController silverAnim;
+    public AnimatorOverrideController ironAnim;
+    public AnimatorOverrideController goldAnim;
+    public AnimatorOverrideController obsidianAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CopperSkin()
+    {
+        GetComponent<Animator>().runtimeAnimatorController = copperAnim as RuntimeAnimatorController;
+    }
+
+    public void SilverSkin()
+    {
+        GetComponent<Animator>().runtimeAnimatorController = silverAnim as RuntimeAnimatorController;
+    }
+
+    public void IronSkin()
+    {
+        GetComponent<Animator>().runtimeAnimatorController = ironAnim as RuntimeAnimatorController;
+    }
+
+    public void goldSkin()
+    {
+        GetComponent<Animator>().runtimeAnimatorController = goldAnim as RuntimeAnimatorController;
+    }
+
+    public void ObsidianSkin()
+    {
+        GetComponent<Animator>().runtimeAnimatorController = obsidianAnim as RuntimeAnimatorController;
+    }
+
+
+}
