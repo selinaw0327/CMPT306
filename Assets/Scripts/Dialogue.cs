a16e534c58b30ed635e54654b0972a832d06cfd5
Zhenzhong Zhou <bob.zhou@usask.ca>
2020-11-15 00:29:58 -0600

    delete two prefabs and add some dialogue text

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialogue : MonoBehaviour
+{
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    private int index;
+    public float typingSpeed;
+
+    void Start()
+    {
+        StartCoroutine(Type());
+    }
+
+    IEnumerator Type()
+    {
+        foreach (char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(0.02f);
+        }
+    }
+}
