ffcbfc9b189f06c0a48d379dd7a6179fa8eaf606
Zhenzhong Zhou <bob.zhou@usask.ca>
2020-11-15 01:42:02 -0600

    add continue button and create dialogue prefab folder

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -10,9 +10,19 @@ public class Dialogue : MonoBehaviour
     private int index;
     public float typingSpeed;
 
+    public GameObject continueButton;
+
     void Start()
     {
         StartCoroutine(Type());
+    }
+
+    void Update()
+    {
+        if (textDisplay.text == sentences[index])
+        {
+            continueButton.SetActive(true);
+        }
     }
 
     IEnumerator Type()
@@ -21,6 +31,22 @@ public class Dialogue : MonoBehaviour
         {
             textDisplay.text += letter;
             yield return new WaitForSeconds(0.02f);
+        }
+    }
+
+    public void NextSentence()
+    {
+        continueButton.SetActive(false);
+
+        if (index < sentences.Length - 1)
+        {
+            index ++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        } else
+        {
+            textDisplay.text = "";
+            continueButton.SetActive(false);
         }
     }
 }
