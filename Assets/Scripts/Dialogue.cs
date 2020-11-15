163dfe548bb70c612d97580faea7e2f1cdf8a332
Alyanna Rabanal <avr240@usask.ca>
2020-11-15 12:43:44 -0600

    Revert "add continue button and create dialogue prefab folder"
    This reverts commit ffcbfc9b189f06c0a48d379dd7a6179fa8eaf606.

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -10,19 +10,9 @@ public class Dialogue : MonoBehaviour
     private int index;
     public float typingSpeed;
 
-    public GameObject continueButton;
-
     void Start()
     {
         StartCoroutine(Type());
-    }
-
-    void Update()
-    {
-        if (textDisplay.text == sentences[index])
-        {
-            continueButton.SetActive(true);
-        }
     }
 
     IEnumerator Type()
@@ -31,22 +21,6 @@ public class Dialogue : MonoBehaviour
         {
             textDisplay.text += letter;
             yield return new WaitForSeconds(0.02f);
-        }
-    }
-
-    public void NextSentence()
-    {
-        continueButton.SetActive(false);
-
-        if (index < sentences.Length - 1)
-        {
-            index ++;
-            textDisplay.text = "";
-            StartCoroutine(Type());
-        } else
-        {
-            textDisplay.text = "";
-            continueButton.SetActive(false);
         }
     }
 }
