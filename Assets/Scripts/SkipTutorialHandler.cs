d03055015888c673726d8765561af644ca1a5fc1
yiw251 <yiw251@mail.usask.ca>
2020-11-16 16:47:48 -0600

    Added skip tutorial button

---
+++ Assets/Scripts/SkipTutorialHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkipTutorialHandler : MonoBehaviour
+{
+    public void disableDialogue(){
+
+        // disable DialogueSystem gameobject
+    
+        GameObject diagloue = GameObject.Find("DialogueSystem");
+
+        diagloue.SetActive(false);
+    }
+}
