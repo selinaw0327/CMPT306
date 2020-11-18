9289f6f5f4f91e281da6be5e14992e138dccbf6c
Alyanna Rabanal <avr240@usask.ca>
2020-11-17 20:17:51 -0600

    added prompt when going to the next level

---
+++ Assets/Prompt.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prompt : MonoBehaviour
+{
+    public void No()
+    {
+        GameObject.Find("Exit").GetComponent<Exit>().prompt = false;
+        Destroy(GameObject.Find("Prompt"));
+    }
+
+    public void Yes()
+    {
+        GameObject.Find("Exit").GetComponent<Exit>().NextLevel();
+        Destroy(GameObject.Find("Prompt"));
+    }
+}
