e0ef9a24bed8e58b203efdbd49181db1e253d969
Zhenzhong Zhou <bob.zhou@usask.ca>
2020-11-27 20:14:13 -0600

    add save sound volume

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -1,32 +1,89 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Audio;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
 {
-    public AudioMixer audioMixer;
-    public AudioSource audioSource;
+    //public AudioMixer audioMixer;
+    //public AudioSource audioSource;
 
-    private float BGMvolume = 1f;
+    private static readonly string FristPlay = "FirstPlay";
+    private static readonly string BackgroundPref = "BackgroundPref";
+    private static readonly string SoundEffectsPref = "SoundEffectsPref";
+    private int firstPlayInt;
+    public Slider backgroundSlider, soundEffectsSlider;
+    private float backgroundFloat, soundEffectsFloat;
+    public AudioSource[] backgroundAudio;
+    public AudioSource[] soundEffectsAudio;
 
-    private void Start()
+    void Start()
     {
-        audioSource.Play();
+        firstPlayInt = PlayerPrefs.GetInt(FristPlay);
+
+        if (firstPlayInt == 0)
+        {
+            backgroundFloat = 0.25f;
+            soundEffectsFloat = .75f;
+            backgroundSlider.value = backgroundFloat;
+            soundEffectsSlider.value = soundEffectsFloat;
+            PlayerPrefs.SetFloat(BackgroundPref, backgroundFloat);
+            PlayerPrefs.SetFloat(SoundEffectsPref, soundEffectsFloat);
+            PlayerPrefs.SetInt(FristPlay, -1);
+        } else
+        {
+            PlayerPrefs.GetFloat(BackgroundPref);
+            backgroundSlider.value = backgroundFloat;
+            PlayerPrefs.GetFloat(SoundEffectsPref);
+            soundEffectsSlider.value = soundEffectsFloat;
+        }
     }
 
-    private void Update()
+    public void SaveSoundSettings()
     {
-        audioSource.volume = BGMvolume;
+        PlayerPrefs.SetFloat(BackgroundPref, backgroundSlider.value);
+        PlayerPrefs.SetFloat(SoundEffectsPref, soundEffectsSlider.value);
     }
 
-    public void UpdateVolume(float volume)
+    private void OnApplicationFocus(bool focus)
     {
-        BGMvolume = volume;
+        if (!focus)
+        {
+            SaveSoundSettings();
+        }
     }
 
-    public void SetVolume (float volume)
+    public void UpdateSound()
     {
-        audioMixer.SetFloat("volume", volume);
+        for (int i = 0; i < backgroundAudio.Length; i++)
+        {
+            backgroundAudio[i].volume = backgroundSlider.value;
+        }
+
+        for (int j = 0; j < soundEffectsAudio.Length; j++)
+        {
+            soundEffectsAudio[j].volume = soundEffectsSlider.value;
+        }
     }
+
+
+    //private float BGMvolume = 1f;
+
+    //private void Start()
+    //{
+    //    audioSource.Play();
+    //}
+
+    //private void Update()
+    //{
+    //    audioSource.volume = BGMvolume;
+    //}
+
+    //public void UpdateVolume(float volume)
+    //{
+    //    BGMvolume = volume;
+    //}
+
+    //public void SetVolume (float volume)
+    //{
+    //    audioMixer.SetFloat("volume", volume);
+    //}
 }
