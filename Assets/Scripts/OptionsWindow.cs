2880e07a55846737120a4893dc77476dcb5b5e1a
Zhenzhong Zhou <bob.zhou@usask.ca>
2020-12-01 20:51:07 -0600

    clean up scene file since adding options menu

---
+++ Assets/Scripts/OptionsWindow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsWindow : MonoBehaviour
+{
+    public GameObject options;
+
+    public void openOptions()
+    {
+        if (options != null)
+        {
+            options.SetActive(true);
+        }
+    }
+
+    public void closeOptions()
+    {
+        if (options != null)
+        {
+            options.SetActive(false);
+        }
+    }
+}
